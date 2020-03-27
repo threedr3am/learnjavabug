d0c615e1e6975e5eb03f4ba7eb115ac975d40fec
“threedr3am” <929811313@qq.com>
2020-03-27 13:20:42 +0800

    feat:添加apache shiro CVE-2020-1957 authentication bypass.

---
+++ shiro/auth-bypass(shiro<1.5.2)/src/main/java/com/threedr3am/bug/shiro/bypass/auth/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.threedr3am.bug.shiro.bypass.auth.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author threedr3am
+ */
+@RestController
+public class LoginController {
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String login(String username, String password) {
+        Subject subject = SecurityUtils.getSubject();
+        try {
+            subject.login(new UsernamePasswordToken(username, password));
+            return "登录成功!";
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            return "登录失败!";
+        }
+
+    }
+}
