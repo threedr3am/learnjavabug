d1da55b54b7227f03e589d0babbc6d57ba9a4178
“threedr3am” <929811313@qq.com>
2020-05-27 01:51:27 +0800

    fix:注释catalina

--- common/src/main/java/org/apache/catalina/startup/Catalina.java
+++ common/src/main/java/org/apache/catalina/startup/Catalina.java
@@ -1,15 +1,15 @@
-package org.apache.catalina.startup;
-
-/**
- * @author threedr3am
- */
-public class Catalina {
-  static {
-    try {
-      System.out.println("run Calc...");
-      Runtime.getRuntime().exec("/System/Applications/Calculator.app/Contents/MacOS/Calculator");
-    } catch (Throwable e) {
-      e.printStackTrace();
-    }
-  }
-}
+//package org.apache.catalina.startup;
+//
+///**
+// * @author threedr3am
+// */
+//public class Catalina {
+//  static {
+//    try {
+//      System.out.println("run Calc...");
+//      Runtime.getRuntime().exec("/System/Applications/Calculator.app/Contents/MacOS/Calculator");
+//    } catch (Throwable e) {
+//      e.printStackTrace();
+//    }
+//  }
+//}
